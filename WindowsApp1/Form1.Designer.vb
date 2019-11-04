39703b6e872406a599108dc25c6c2521233de272
Patrick TELLIER <tellierp@cyberdev.be>
2019-11-04 20:47:53 +0100

    first push

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.BtnActionGo = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(-1, 2)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(799, 21)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'BtnActionGo
+        '
+        Me.BtnActionGo.Location = New System.Drawing.Point(335, 111)
+        Me.BtnActionGo.Name = "BtnActionGo"
+        Me.BtnActionGo.Size = New System.Drawing.Size(75, 23)
+        Me.BtnActionGo.TabIndex = 1
+        Me.BtnActionGo.Text = "Go"
+        Me.BtnActionGo.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(488, 74)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(172, 45)
+        Me.TrackBar1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.BtnActionGo)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents BtnActionGo As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TrackBar1 As TrackBar
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Dim vitesse As Integer
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Increment(vitesse)
+        If ProgressBar1.Value = 100 Then
+            ProgressBar1.Value = 0
+        End If
+    End Sub
+
+    Private Sub BtnActionGo_Click(sender As Object, e As EventArgs) Handles BtnActionGo.Click
+        vitesse = 1
+        If BtnActionGo.Text = "Stop" Then
+            BtnActionGo.Text = "Go"
+            Timer1.Enabled = False
+
+        Else
+            BtnActionGo.Text = "Stop"
+            Timer1.Enabled = True
+        End If
+
+
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        vitesse = TrackBar1.Value
+    End Sub
+End Class
